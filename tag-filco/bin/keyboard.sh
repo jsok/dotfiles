a24f8cceb85b51deff4173933077867f7348a3a2
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2014-08-14 15:55:32 +1000

    filco: Update udev script instructions

--- tag-filco/bin/keyboard.sh
+++ tag-filco/bin/keyboard.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# udev rule to trigger filco keyboard setup
+#
+# Create a file `/etc/udev/rules.d/96-filco-majestouch.rules`
+# With the line:
+#   ACTION=="add" ATTRS{idVendor}=="04d9", ATTRS{idProduct}=="1818", RUN+="/home/jsok/.bin/run-script-in-background /home/jsok/.bin/keyboard.sh"
+#
+# Reload udev rules with:
+#   udevadm control --reload-rules
+#
+# Unplug keyboard and re-attach to confirm it works
+
 sleep 2
 DISPLAY=":0.0"
 HOME=/home/jsok/
