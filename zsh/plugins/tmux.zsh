b66e4890a39383169a7b55b9e4ed871550ac95ae
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2016-02-18 14:49:15 +1100

    zsh: Add tmux precmd plugin which dynamically updates window title

---
+++ zsh/plugins/tmux.zsh
@@ -0,0 +1,17 @@
+# Sets the current tmux window title to either:
+#  * the git repo name
+#  * the current directory name
+
+tmux_title() {
+    if [[ "$TMUX" != "" ]]; then
+        TMUX_TITLE=""
+        if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+            TMUX_TITLE=$(basename `git rev-parse --show-toplevel`)
+        else
+            TMUX_TITLE=$(basename `pwd`)
+        fi
+        tmux rename-window "$TMUX_TITLE"
+    fi
+}
+
+precmd_functions=(${precmd_functions[@]} "tmux_title")
