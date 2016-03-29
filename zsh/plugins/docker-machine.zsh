78c46911857b52daa628bc4f3bbcad56588889fc
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2016-03-29 21:22:58 +1100

    zsh: docker-machine plugin checks for machine status before using

--- zsh/plugins/docker-machine.zsh
+++ zsh/plugins/docker-machine.zsh
@@ -2,7 +2,7 @@ function docker_machine_use() {
     eval $(docker-machine env "$*")
 }
 
-if [ -n "$DOCKER_MACHINE_DEFAULT" ]; then
+if [ -n "$DOCKER_MACHINE_DEFAULT" -a $(docker-machine status "$DOCKER_MACHINE_DEFAULT") == "Running" ]; then
     docker_machine_use $DOCKER_MACHINE_DEFAULT
 fi
 
