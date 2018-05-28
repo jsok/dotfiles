71381264157431856c6a91ab6158a4672c2fcbf0
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2018-05-28 18:12:35 +1000

    python: Add bin to $PATH for --user packages

--- zsh/plugins/python.zsh
+++ zsh/plugins/python.zsh
@@ -1,4 +1,5 @@
 # Python
+export PATH=$(python3 -m site --user-base)/bin:$(python2 -m site --user-base)/bin:$PATH
 export WORKON_HOME=$HOME/.virtualenvs
 export PROJECT_HOME=$HOME/dev
 export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2
