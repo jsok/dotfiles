6c5b9be5af3d611aeccfd18b80d19a030c433151
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2020-12-02 10:57:26 +1100

    python: Use user site instead of virtualenvwrapper

--- zsh/plugins/python.zsh
+++ zsh/plugins/python.zsh
@@ -1,6 +1,2 @@
 # Python
-export PATH=$(python3 -m site --user-base)/bin:$(python2 -m site --user-base)/bin:$PATH
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/dev
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python2
-source /usr/local/bin/virtualenvwrapper.sh
+export PATH=$(python3 -m site --user-base)/bin:$PATH
