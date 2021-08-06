bfc60bc951c829adbc745761e76b52637204fe33
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2021-08-06 14:55:20 +1000

    zsh: gcloud plugin

--- zsh/plugins/gcloud.zsh
+++ zsh/plugins/gcloud.zsh
@@ -1,5 +1,9 @@
 # Google Cloud SDK
 
-if [ -n "${GOOGLE_CLOUD_SDK_ROOT}" ]; then
-    source $GOOGLE_CLOUD_SDK_ROOT/path.zsh.inc
-fi
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
+
+gcp(){
+    local readonly g=( "gcloud" "config" "configurations" )
+    ${g} activate ${1:-$(${g} list |  fzf --header-lines 1 --reverse | cut -d ' ' -f 1)}
+}
