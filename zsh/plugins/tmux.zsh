b83a767ed71c8bf166dd5c50af3711fa9b723f15
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2023-12-15 10:37:50 +1100

    tmux,zsh: rename window changes

--- zsh/plugins/tmux.zsh
+++ zsh/plugins/tmux.zsh
@@ -1,18 +1,21 @@
-# Deprecated: Use tmux automatic-rename-format instead
 # Sets the current tmux window title to either:
 #  * the git repo name
 #  * the current directory name
+# Copied from:
+# https://github.com/sei40kr/zsh-tmux-rename/blob/master/zsh-tmux-rename.plugin.zsh
 
-tmux_title() {
-    if [[ "$TMUX" != "" ]]; then
-        TMUX_TITLE=""
-        if [ git rev-parse --is-inside-work-tree >/dev/null 2>&1 == "true" ]; then
-            TMUX_TITLE=$(basename `git rev-parse --show-toplevel`)
-        else
-            TMUX_TITLE=$(basename `pwd`)
-        fi
-        tmux rename-window "$TMUX_TITLE"
-    fi
+rename-tmux-window() {
+  [[ -z "$TMUX" ]] && return
+
+  LANG=en_US.UTF-8 vcs_info
+  if [[ -n "$vcs_info_msg_1_" ]]; then
+    tmux rename-window "${vcs_info_msg_1_}"
+  else
+    tmux rename-window "${PWD##*/}"
+  fi
 }
 
-precmd_functions=(${precmd_functions[@]} "tmux_title")
+autoload -U add-zsh-hook
+add-zsh-hook chpwd rename-tmux-window
+
+[[ -z "$TMUX" ]] || rename-tmux-window
