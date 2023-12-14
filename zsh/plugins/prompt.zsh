9cdbc3e87304ac67cfb4380297f4a0420d7381f4
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2023-12-15 10:29:23 +1100

    zsh: tweak prompt

--- zsh/plugins/prompt.zsh
+++ zsh/plugins/prompt.zsh
@@ -1,11 +1,3 @@
 # Customised prompt with git info
-
-autoload -Uz vcs_info
-precmd() { vcs_info }
-zstyle ':vcs_info:*' enable git
-
-# Just show the current branch name
-zstyle ':vcs_info:git*' formats '%b '
-
 setopt PROMPT_SUBST
-PROMPT='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%M:"}%{$fg_bold[green]%}${vcs_info_msg_0_}%{$reset_color%}%~> '
+PROMPT='${SSH_CONNECTION+"%{$fg_bold[green]%}%n@%M:"}%{$fg_bold[green]%}${vcs_info_msg_0_}%{$reset_color%}%~%{$fg_bold[green]%}>%{$reset_color%} '
