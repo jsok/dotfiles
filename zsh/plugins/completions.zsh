231afdfa4b97c348bc4988773f54960bc6f97c73
Jonathan Sokolowski <jonathan.sokolowski@gmail.com>
2023-12-15 10:28:41 +1100

    zsh: load site functions

--- zsh/plugins/completions.zsh
+++ zsh/plugins/completions.zsh
@@ -1,5 +1,11 @@
 # Load zsh-users/zsh-completions
 
+ZSH_SITE_FUNCTIONS=${ZSH_SITE_FUNCTIONS:-$(brew --prefix)/share/zsh/site-functions}
+
+if [ -n "$ZSH_SITE_FUNCTIONS" ]; then
+    fpath=($ZSH_SITE_FUNCTIONS $fpath)
+fi
+
 ZSH_COMPLETIONS_ROOT=${ZSH_COMPLETIONS_ROOT:-$(brew --prefix)/share/zsh-completions}
 
 if [ -n "$ZSH_COMPLETIONS_ROOT" ]; then
